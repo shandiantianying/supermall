16cf24d705ef4e08a32510208dc40435e1f17da3
zhou button <zhoubutton@zhoudeMacBook-Pro.local>
2020-02-26 22:42:42 +0800

    添加数据

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,12 +1,37 @@
 <template>
   <div>
-   <h1>分类</h1>
+   <h1>分类111</h1>
+   <SubCategory @emitMethods="faterMetherod" ref="child"></SubCategory>
+   <button @click="callsubmethod">父组件调用子组件</button>
+   {{submsg}}
   </div>
 </template>
 
 <script>
+import SubCategory from './SubCategory'
 export default {
-  name: 'Category'
+  name: 'Category',
+  submsg:'',
+  components:{
+    SubCategory
+  },
+  methods:{
+    faterMetherod(params){
+      console.log(params)
+      alert(JSON.stringify(params))
+    },
+    callsubmethod(){
+      console.log(this.$refs.child.subctgmsg);
+      console.log(this.$refs.child);
+      
+      this.$refs.child.subCatey('父组件传入参数');
+    }
+  },
+  computed:{
+    submsg(){
+      // return this.$refs.child.subctgmsg;
+    }
+  }
 }
 </script>
 
---
+++ src/views/category/SubCategory.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class='SubCategory'>
+      <h1>子组件</h1>
+
+      <button @click="clickMethod">子组件调用父组件方法</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'SubCategory',
+    data(){
+        return {
+            subctgmsg:'Subcategory message'
+        }
+    },
+    mounted(){
+        this.$emit('emitMethods',{"name":123})
+    },
+    methods:{
+        clickMethod(){
+            this.$emit('emitMethods','{"name":123}')
+        },
+        subCatey(params){
+            console.log(params)
+            console.log('父组件调用子组件方法。')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
